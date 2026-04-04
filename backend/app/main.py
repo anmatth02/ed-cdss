8dc813dbefbb7ec3374755d4746f91867e36517b
anmatth02 <matheou.an@gmail.com>
2026-04-04 19:47:53 +0300

    fix main

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,13 +4,21 @@ from .database import Base, engine
 from .routers import cases
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "https://ed-cdss.vercel.app",
+    "https://ed-cdss-3iikypohd-anmatth02s-projects.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 Base.metadata.create_all(bind=engine)
 
 app.include_router(
